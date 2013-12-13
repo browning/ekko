ec89cf8fae70120f3a047eae7d629065f5e28066
browning <bwbrowning@gmail.com>
2013-12-12 22:26:47 -0600

    Generate public / private key and store inside of html5 local storage

--- js/app.js
+++ js/app.js
@@ -16,4 +16,21 @@ ekkoApp.config(['$routeProvider',
 
 function KeysCtrl($scope, $location, $routeParams) {
   $scope.name = $routeParams.name;
+
+  if ( !localStorage['pk'] ) {
+    $scope.message = "Creating a key for you"
+    var nacl = nacl_factory.instantiate();
+    mykeys = nacl.crypto_box_keypair();
+    $scope.pk = mykeys.boxPk;
+    $scope.sk = mykeys.boxSk;
+
+    localStorage['pk'] = JSON.stringify(mykeys.boxPk);
+    localStorage['sk'] = JSON.stringify(mykeys.boxSk);
+  }
+  else {
+    $scope.message = "You already have keys"
+    $scope.pk = localStorage['pk']
+
+  }
+
 }

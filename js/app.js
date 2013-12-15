58add6499bbddca4da967c0dc88a3c1293e696a6
browning <bwbrowning@gmail.com>
2013-12-14 21:38:56 -0600

    connect to onramp server when on the messages page

--- js/app.js
+++ js/app.js
@@ -47,6 +47,50 @@ function MsgsCtrl($scope, $location, $routeParams) {
   $scope.select_chan = function(chan) {
     $scope.displayed_channel = chan;
   }
+
+  // peerjs stuff
+  var peer = P.create()
+
+  var webSocketPeer = peer.to('ws://localhost:20500/');
+  console.log('connecting to onramp');
+  var peer_list = new Array();
+
+  // Listen to the messages the onramp server sends
+  webSocketPeer.on('message', function(message){
+      console.log(message);
+          // If we recieve a remote address, add it to peer list
+          if(message === "remote address"){
+                  var peerAddress = arguments[1];
+                  peer_list.push(peerAddress);
+                  console.log('Potential peer: ' + peerAddress);
+          }
+  });
+
+  webSocketPeer.on('connection', handleRtcConnection);
+
+  function handleRtcConnection(webRtcPeer){
+          // When ever another browser connects, listen for messages
+          webRtcPeer.on('message', function(message){
+                  // Output the message
+                  console.log('I received: ' + message);
+          });        
+  }
+
+  function broadcast(msg) {
+    for(i=0; i<peer_list.length; i++) {
+      webRtcPeer = webSocketPeer.to(peer_list[i]);
+      console.log('sending ' + msg + ' to ' + peer_list[i]);
+      webRtcPeer.on('open', function() {
+        this.send(msg);
+      });
+    }
+  }
+
+  function send_message(msg) {
+    broadcast(msg);
+  }
+
+
 }
 
 function KeysCtrl($scope, $location, $routeParams) {
